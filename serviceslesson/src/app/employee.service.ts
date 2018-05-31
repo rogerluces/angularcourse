15cd5acd810ff4d8da1e667083259a32613a5e6b
RogerLuces <Roger.Luces@gmail.com>
2018-05-31 17:47:00 -0400

    Clase de Observables y Suscribe

--- serviceslesson/src/app/employee.service.ts
+++ serviceslesson/src/app/employee.service.ts
@@ -1,17 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IEmployee } from './employee';
+//import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class EmployeeService {
 
-  constructor() { }
+  private _url: string = "/assets/data/employees.json";
 
-  getEmployees(){
-    return [
+  constructor(private http: HttpClient) { }
+  
+  getEmployees(): Observable<IEmployee[]>{
+      return this.http.get<IEmployee[]>(this._url);
+
+    /*return [
       {"id": 1, "name":"Roger Luces", "age": 46, "active":true},
       {"id": 2, "name":"Jorge Ramirez", "age": 24, "active":true},
       {"id": 3, "name":"Jaime Aguilera", "age": 32, "active":true},
       {"id": 4, "name":"Seba Ahumada", "age": 28, "active":true},
       {"id": 5, "name":"Otro Ahi", "age": 55, "active":false},
-    ];
+    ];*/
   }
 }
